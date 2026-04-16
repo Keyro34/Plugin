c896356811e388e69fe0fb77a6c2071b72d65a52
Keyro <55703261+Keyro34@users.noreply.github.com>
2026-04-16 17:54:54 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- online_mod.js
+++ online_mod.js
@@ -267,7 +267,7 @@
         if (name === 'iframe') return user_proxy2;
         if (name === 'lumex') return proxy_secret;
         if (name === 'rezka') return user_proxy2;
-        if (name === 'rezka2') return user_proxy2;
+        if (name === 'rezka2') return user_proxy1;
         if (name === 'kinobase') return proxy_secret;
         if (name === 'collaps') return proxy_secret;
         if (name === 'cdnmovies') return proxy_secret;
@@ -2334,7 +2334,7 @@
               checkErrorForm(str);
             }
 
-            if (error_message) component.empty(error_message);else component.empty(network.errorDecode(a, c));
+            if (error_message) component.empty(error_message);else if (callback) callback([], false, query);else component.empty(network.errorDecode(a, c));
           }, postdata, {
             dataType: 'text',
             withCredentials: logged_in,
