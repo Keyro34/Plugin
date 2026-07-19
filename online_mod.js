e1b6b3ad7f556cb8579b0dca584933845ffe2786
Keyro <55703261+Keyro34@users.noreply.github.com>
2026-07-19 20:33:45 +0200

    Test

--- online_mod.js
+++ online_mod.js
@@ -274,7 +274,7 @@
         if (name === 'iframe') return user_proxy2;
         if (name === 'lumex') return proxy_secret;
         if (name === 'rezka') return proxy_secret;
-        if (name === 'rezka2') return proxy_secret;
+        if (name === 'rezka2') return user_proxy1;
         if (name === 'kinobase') return proxy_secret;
         if (name === 'collaps') return proxy_secret;
         if (name === 'cdnmovies') return proxy_secret;
